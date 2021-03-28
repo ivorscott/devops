8394fbe72214b2d4f05329c908a98689e9671993
Ivor Scott <ivor@devpie.io>
2021-03-28 21:02:06 +0200

    feat: add acm certificate automation

--- eks-traefik/setup_ingress.sh
+++ eks-traefik/setup_ingress.sh
@@ -9,11 +9,19 @@ metadata:
     kubernetes.io/ingress.class: alb
     alb.ingress.kubernetes.io/scheme: internet-facing
     alb.ingress.kubernetes.io/target-type: ip
+    alb.ingress.kubernetes.io/actions.ssl-redirect: '{"Type": "redirect", "RedirectConfig": { "Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}'
+    alb.ingress.kubernetes.io/backend-protocol: HTTP
+    alb.ingress.kubernetes.io/certificate-arn: arn:aws:acm:eu-central-1:554897346438:certificate/ef9e7fcb-11e6-408b-a57d-b3e914226791
 spec:
   rules:
     - host: $1
       http:
         paths:
+          # HTTP to HTTPS redirect entry
+          - path: /*
+            backend:
+              serviceName: ssl-redirect
+              servicePort: use-annotation
           - path: /*
             backend:
               serviceName: service-2048
